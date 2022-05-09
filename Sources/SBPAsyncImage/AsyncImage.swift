58e39678f9c47f7c2103d66495d9d830dee9818d
yutailang0119 <yutailang0119@gmail.com>
2022-05-09 23:07:02 +0900

    Remove typealias as AsyncImage

--- Sources/SBPAsyncImage/AsyncImage.swift
+++
@@ -1,5 +0,0 @@
-@available(iOS, deprecated: 15.0, renamed: "SwiftUI.AsyncImage")
-@available(macOS, deprecated: 12.0, renamed: "SwiftUI.AsyncImage")
-@available(tvOS, deprecated: 15.0, renamed: "SwiftUI.AsyncImage")
-@available(watchOS, deprecated: 8.0, renamed: "SwiftUI.AsyncImage")
-public typealias AsyncImage = BackportAsyncImage
